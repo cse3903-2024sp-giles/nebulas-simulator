a6142733f6a885ebe152e047f4475f9b73f80d0a
18776 <18776@WINDELL-GQFHPOR.lan>
2024-02-06 14:36:12 -0500

    word

--- giles-nebulas-simulator/src/main/java/nebulas/simulator/Word.java
+++ giles-nebulas-simulator/src/main/java/nebulas/simulator/Word.java
@@ -1,5 +1,5 @@
 package nebulas.simulator;
-
+import java.util.BitSet; 
 /**
  * Word - The smallest addressable piece of data in the simulator.
  *<p>
@@ -8,21 +8,64 @@ package nebulas.simulator;
  *<p>
  *@see Machine*/
 public interface Word{
-
-
-    Word add();
+    
 
     //We might add these later
     //byte getFirstByte();
 
     //byte getSecondByte();
 
-    boolean getBit(int position);
+    boolean getBit(int position);   //get the bit on the position 
 
-    boolean setBit(int position, boolean value);
+    boolean setBit(int position, boolean value);    //set the bit on the position to the value
 
-    boolean equals(Word b);
+    boolean equals(Word b); //check if two words are equal
 
+}
 
+class Word1 implements Word {
+    
+    BitSet w = new BitSet();
+    .
+    //Word1 class constructor, input should be string, then transform into bitset
+    Word(String input){
+        String pattern = "[01]+";
+        if (input.matches(pattern)) {
+            String[] str = input.split("");
+            for(int i = 0;i < input.length();i++) {
+                if(str[i].equals("1")) {
+                    this.w.set(i);
+                }
+            }
+        } else {
+            System.out.println("String does not contain only 1s and 0s.");
+        }              
+    }
+    
+    public boolean getBit(int position) {
+        if(position < 17 && position >= 0) {
+            return this.w.get(position-1);
+        }else {
+            System.out.println("error position");
+            return false;
+        }        
+    }
 
+    public boolean setBit(int position, boolean value) {
+        if(position < 17 && position >= 0) {
+            if (value) {
+                this.w.set(position - 1); // start from 0
+            }else {
+                this.w.clear(position - 1); // start from 0
+            }
+            return true;
+        }else {
+            System.out.println("error position");
+            return false;
+        }
+    }
+
+    public boolean equals(Word b) {
+        return this.w.equals(b.w);
+    }
 }
