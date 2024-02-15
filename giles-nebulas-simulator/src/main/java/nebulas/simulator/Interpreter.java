f3470a737ecad9d18aa7a13a095ca9d38d37d570
Seth <n0bud@protonmail.com>
2024-02-15 13:35:04 -0500

    interpreter finish

--- giles-nebulas-simulator/src/main/java/nebulas/simulator/Interpreter.java
+++ giles-nebulas-simulator/src/main/java/nebulas/simulator/Interpreter.java
@@ -1,5 +1,8 @@
 package nebulas.simulator;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *Interpreter - parses instructions passed and operates on the machine to setup for the next state.
  *
@@ -32,7 +35,7 @@ public class Interpreter{
         currentInstruction = m.cpu;
         
 
-        switch (currentInstruction.toString().substring(0, 4)) {
+        switch (currentInstruction.bitsToString(15, 12)) {
             case "0001": //add
                 add();
                 break;
@@ -202,18 +205,70 @@ public class Interpreter{
     
     private void brx(){
         
+        String page = m.pc.bitsToString(9, 15);
+        String index = currentInstruction.bitsToString(0, 8);
+
+        Word address = new Word1(page + index);
+
+        //if ANY
+        if(
+            (currentInstruction.getBit(11) && m.ccr.getN())
+            ||
+            (currentInstruction.getBit(10) && m.ccr.getZ())
+            ||
+            (currentInstruction.getBit(9) && m.ccr.getP())
+        ){
+            //branch
+            //
+            m.pc = address;
+        }        
     }
     
     private void dbug(){
+        
+        System.out.println("PC: " + m.pc.bitsToString(0, 15));
+        
+        for( int i = 0; i < 8; i++){
+            System.out.println("Register " + Integer.toString(i) + " : " + m.registers.getRegister(i).bitsToString(0, 15));
+        }
+
+        System.out.println("CCR: N[" + String.valueOf(m.ccr.getN()) + "] " + "Z[" + String.valueOf(m.ccr.getZ()) + "] " + "P[" + String.valueOf(m.ccr.getP()) + "]");
         
     }
     
     private void jsr(){
         
+        String page = m.pc.bitsToString(9, 15);
+        String index = currentInstruction.bitsToString(0, 8);
+
+        Word address = new Word1(page + index);
+
+        if (currentInstruction.getBit(11)) {
+            //jump
+            m.registers.setRegister(7, m.pc);
+            m.pc = address;
+        }else{
+            m.pc = address;
+        }
+        
     }
     
     
     private void jsrr(){
+        
+        Word br = m.registers.getRegister(currentInstruction.bitsToInt(6, 8));
+
+        Word index = new Word1(currentInstruction.bitsToInt(0, 5));
+        Word address = br.add(index);
+
+        if (currentInstruction.getBit((11))) {
+            //jump 
+            m.registers.setRegister(7, m.pc);
+            m.pc = address;
+            
+        }else{
+            m.pc = address;
+        }
         
     }
 
@@ -295,6 +350,25 @@ public class Interpreter{
     
     private void lea(){
         
+        int dr = currentInstruction.bitsToInt(9, 11);
+        String page = m.pc.bitsToString(9, 15);
+        String index = currentInstruction.bitsToString(0, 8);
+
+        Word value = new Word1(page + index);
+        
+
+        m.registers.setRegister(dr, value);
+          //set CCR
+          if(value.sign() == 1){
+            m.ccr.setP();
+        }else if (value.sign() == -1) {
+            m.ccr.setN();
+        }else{
+            m.ccr.setZ();
+        }        
+        
+
+        
     }
     
     private void not(){
@@ -321,22 +395,141 @@ public class Interpreter{
     
     private void ret(){
         
-    }
-    
-    private void st(){
+        m.pc = m.registers.getRegister(7);
         
     }
     
+    private void st(){
+
+        Word value  = m.registers.getRegister(currentInstruction.bitsToInt(9, 11)); 
+        
+        String page = m.pc.bitsToString(9, 15);
+        String index = currentInstruction.bitsToString(0, 8);
+
+        Word address = new Word1(page + index);
+
+        m.memory.setWord(address, value);
+    }
+    
     private void sti(){
+        Word value = m.registers.getRegister(currentInstruction.bitsToInt(9, 11));
+
+        String page = m.pc.bitsToString(9, 15);
+        String index = currentInstruction.bitsToString(0, 8);
+
+        Word address = new Word1(page + index);
+
+        m.memory.setWord(address, value);
         
     }
     
     private void str(){
         
+        Word value = m.registers.getRegister(currentInstruction.bitsToInt(9, 11));
+
+        Word br = m.registers.getRegister(currentInstruction.bitsToInt(6, 8));
+        
+        Word index = new Word1(currentInstruction.bitsToString(0, 5));
+        Word address = br.add(index);
+
+        m.memory.setWord(address, value);
+        
     }
     
     private void trap(){
         
+        String vector = currentInstruction.bitsToString(0, 7);
+
+
+        switch (vector) {
+            case "00100001": //out
+                
+                Word reg = m.registers.getRegister(0);
+                int cha = reg.bitsToInt(0, 7);
+
+                char value = (char) cha;
+
+                System.out.print(value);
+                
+                break;
+            case "00100010": //puts
+                
+                Word address = m.registers.getRegister(0);
+                
+                Word charVal = m.memory.getWord(address);
+
+
+
+                //while not a null char
+                while ((char) charVal.bitsToInt(0, 7) != '\0') {
+                    
+                    System.out.print((char) charVal.bitsToInt(0, 7));
+                    
+                    //incr address
+                    address = address.add(new Word1(1));
+                    
+                    charVal = m.memory.getWord(address);
+                    
+                }
+                break;
+            case "00100011": //in
+                
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a character: ");
+
+                char inputChar = scanner.next().charAt(0);
+                
+                System.out.println("Entered: " + inputChar);
+                
+
+                //take this char and put in r0
+
+                Word result = new Word1((int) inputChar);
+                
+                m.registers.setRegister(0, result);
+                
+                scanner.close();
+                
+                break;
+            case "00100101": //halt
+                             //
+                System.out.println("[HALT]");
+                System.exit(0);
+                break;
+            case "00110001": //outN
+                
+                int val = m.registers.getRegister(0).bitsToInt(0, 15);
+                break;
+            case "00110011": //Inn
+                
+                Scanner scanner2 = new Scanner((System.in));
+                System.out.print("Enter a decimal number: ");
+
+                int num = scanner2.nextInt(0);
+                
+                System.out.println("Entered: " + num);
+                
+                //store the 
+                //
+                
+                Word res = new Word1(num);
+                
+                m.registers.setRegister(0, res);
+
+                
+
+                scanner2.close();
+
+                break;
+            case "01000011": //rnd
+                
+                Random random = new Random();
+                int randomInt = random.nextInt(65501);
+                m.registers.setRegister(0, new Word1(randomInt));
+                break;
+            default:
+                break;
+        }
     }
 
 }
