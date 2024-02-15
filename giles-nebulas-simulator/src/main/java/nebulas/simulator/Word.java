a426e744ff2bfa24677ef2c75bed0876bb5d7adf
zining352 <fengxining02@gmail.com>
2024-02-14 21:02:17 -0500

    2024/2/14 21:02

--- giles-nebulas-simulator/src/main/java/nebulas/simulator/Word.java
+++ giles-nebulas-simulator/src/main/java/nebulas/simulator/Word.java
@@ -1,17 +1,22 @@
 package nebulas.simulator;
-import java.util.BitSet; 
+
+import java.util.BitSet;
+
 /**
  * Word - The smallest addressable piece of data in the simulator.
- *<p>
- *Words are the smallest addressable type. Contains methods for manipulating a specific point in memory.
- *Memory is an array of these words. The PC is also type word.
- *<p>
- *@see Machine*/
-public interface Word{
+ * <p>
+ * Words are the smallest addressable type. Contains methods for manipulating a
+ * specific point in memory. Memory is an array of these words. The PC is also
+ * type word.
+ * <p>
+ *
+ * @see Machine
+ */
+public interface Word {
 
-    boolean getBit(int position);   //get the bit on the position 
+    boolean getBit(int position); //get the bit on the position
 
-    boolean setBit(int position, boolean value);    //set the bit on the position to the value
+    boolean setBit(int position, boolean value); //set the bit on the position to the value
 
     boolean equals(Word b); //check if two words are equal
 
@@ -24,48 +29,96 @@ public interface Word{
 }
 
 class Word1 implements Word {
-    
-    BitSet w = new BitSet();
-    .
+
+    BitSet w = new BitSet(16);
+
     //Word1 class constructor, input should be string, then transform into bitset
-    Word(String input){
+    public Word1() {
+        this.w = new BitSet(16);
+    }
+
+    public Word1(String input) {
         String pattern = "[01]+";
         if (input.matches(pattern)) {
-            String[] str = input.split("");
-            for(int i = 0;i < input.length();i++) {
-                if(str[i].equals("1")) {
+
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == '1') {
                     this.w.set(i);
                 }
             }
         } else {
             System.out.println("String does not contain only 1s and 0s.");
-        }              
-    }
-    
-    public boolean getBit(int position) {
-        if(position < 17 && position >= 0) {
-            return this.w.get(position-1);
-        }else {
-            System.out.println("error position");
-            return false;
-        }        
+        }
     }
 
-    public boolean setBit(int position, boolean value) {
-        if(position < 17 && position >= 0) {
-            if (value) {
-                this.w.set(position - 1); // start from 0
-            }else {
-                this.w.clear(position - 1); // start from 0
-            }
-            return true;
-        }else {
+    @Override
+    public boolean getBit(int position) {
+        if (position < 17 && position >= 0) {
+            return this.w.get(position);
+        } else {
             System.out.println("error position");
             return false;
         }
     }
 
+    @Override
+    public boolean setBit(int position, boolean value) {
+        if (position < 17 && position >= 0) {
+            if (value) {
+                this.w.set(position); // start from 0
+            } else {
+                this.w.clear(position); // start from 0
+            }
+            return true;
+        } else {
+            System.out.println("error position");
+            return false;
+        }
+    }
+
+    @Override
     public boolean equals(Word b) {
-        return this.w.equals(b.w);
+        Word1 wordB = (Word1) b;
+
+        return this.w.equals(wordB.w);
+    }
+
+    @Override
+    public Word add(Word b) {
+        Word1 wordB = (Word1) b;
+        Word1 wordA = new Word1();
+
+        int lengthA = this.w.length();
+        int lengthB = wordB.w.length();
+
+        wordA.w = (BitSet) this.w.clone();
+        for (int i = 0; i < lengthB; i++) {
+            boolean bit = wordB.w.get(i);
+            wordA.setBit(lengthA + i, bit);
+        }
+
+        return wordA;
+    }
+
+    @Override
+    public Word and(Word b) {
+        Word1 wordA = new Word1();
+        wordA.w = (BitSet) this.w.clone();
+        Word1 wordB = (Word1) b;
+
+        wordA.w.and(wordB.w);
+
+        return wordA;
+    }
+
+    @Override
+    public Word not(Word b) {
+        Word1 wordA = new Word1();
+        wordA.w = (BitSet) this.w.clone();
+        Word1 wordB = (Word1) b;
+
+        wordA.w.andNot(wordB.w);
+
+        return wordA;
     }
 }
