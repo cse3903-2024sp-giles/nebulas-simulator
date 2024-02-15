f29cad0fa2ada3f64a33f7558bc9bd1e5a79dbef
zining352 <fengxining02@gmail.com>
2024-02-14 22:18:42 -0500

    n

--- giles-nebulas-simulator/src/test/java/nebulas/simulator/wordTest.java
+++ giles-nebulas-simulator/src/test/java/nebulas/simulator/wordTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class wordTest {
     //test if two word which are created by same class [Word1()] are same
     @Test
-    public void testWord1() {
+    public void testWord1_1() {
         Word a = new Word1();
 
         Word b = new Word1();
@@ -15,12 +15,38 @@ public class wordTest {
         assertEquals(a, b);
     }
 
-    //test if
+    //test if "a" initialized successfully
     @Test
-    public void testWord2() {
+    public void testWord1_2() {
+        Word a = new Word1();
+
+        assertEquals(a.getBit(0), false);
+        assertEquals(a.getBit(15), false);
+    }
+
+    //test if "a" initialized successfully
+    @Test
+    public void testWord1_3() {
         Word a = new Word1("1");
 
         assertEquals(a.getBit(0), true);
+    }
+
+    //test if "a" initialized successfully
+    @Test
+    public void testWord1_4() {
+        Word a = new Word1("1000000100000001");
+
+        assertEquals(a.getBit(0), true);
+        assertEquals(a.getBit(7), true);
+        assertEquals(a.getBit(15), true);
+    }
+
+    @Test
+    public void testgetBit_1() {
+        Word a = new Word1("1000000100000001");
+
+        assertEquals(a.getBit(17), false);
     }
 
 }
