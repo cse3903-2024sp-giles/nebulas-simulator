5ab96af731b61fe0180d3ac9f0873baeffd5c7e8
Seth <n0bud@protonmail.com>
2024-02-14 20:06:25 -0500

    sim

---
+++ giles-nebulas-simulator/src/main/java/nebulas/simulator/Simulator.java
@@ -0,0 +1,17 @@
+package nebulas.simulator;
+
+/**
+ *Simulator - The class that sets up and runs the implementation *
+ *<p>
+ *Constructs the machine and loader and then runs the interpreter on the state until complete
+ *<p>
+ **/
+public class Simulator{
+
+
+    /**
+     *Runs the sim once it is constructed  */
+    public void run(){
+
+    }
+}
