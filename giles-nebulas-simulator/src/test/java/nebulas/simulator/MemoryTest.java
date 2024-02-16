99c5a2c6f75f01a0aa706152272daa547be7c7a9
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2024-02-16 18:08:21 -0500

    memory max tests

--- giles-nebulas-simulator/src/test/java/nebulas/simulator/MemoryTest.java
+++ giles-nebulas-simulator/src/test/java/nebulas/simulator/MemoryTest.java
@@ -24,4 +24,23 @@ public class MemoryTest {
         assertTrue(memory.getWord(address).equals(new Word1(10)));
     }
 
+    @Test
+    public void testMemory_Addr_Max_String() {
+        Word address = new Word1("1111111111111111");
+        Memory memory = new Memory1();
+        memory.setWord(address, new Word1(10000));
+
+        assertTrue(memory.getWord(address).equals(new Word1(10000)));
+    }
+
+    @Test
+    public void testMemory_Addr_Max_Int() {
+        // TODO: 65536 and above shouldn't work?
+        Word address = new Word1(65536);
+        Memory memory = new Memory1();
+        memory.setWord(address, new Word1(10));
+
+        assertTrue(memory.getWord(address).equals(new Word1(10)));
+    }
+
 }
