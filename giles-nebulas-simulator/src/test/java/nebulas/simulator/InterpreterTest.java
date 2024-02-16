39a46d7accecf5fc4fb95fd0b5486ced475e16d6
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2024-02-16 00:13:01 -0500

    interpreter add test

---
+++ giles-nebulas-simulator/src/test/java/nebulas/simulator/InterpreterTest.java
@@ -0,0 +1,26 @@
+package nebulas.simulator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class InterpreterTest {
+    @Test
+    public void testInterpreterAdd() {
+        Machine1 machine = new Machine1(Machine.MODE.QUIET);
+        machine.registers.setRegister(0, new Word1(10));
+        machine.registers.setRegister(1, new Word1(20));
+        machine.pc = new Word1("0000 0000 0000 0001");
+
+        Interpreter interpreter = new Interpreter(machine);
+        interpreter.step();
+
+        Word expected = new Word1(30);
+        Word actual = machine.registers.getRegister(2);
+        System.out.println(expected);
+        System.out.println(actual);
+
+        assertTrue(actual.equals(expected));
+    }
+}
