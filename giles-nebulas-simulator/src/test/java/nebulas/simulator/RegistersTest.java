daf03ea01553df0572ed2a548d422a4b3118c5ef
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2024-02-16 17:02:41 -0500

    fixed register test

--- giles-nebulas-simulator/src/test/java/nebulas/simulator/RegistersTest.java
+++ giles-nebulas-simulator/src/test/java/nebulas/simulator/RegistersTest.java
@@ -18,13 +18,23 @@ public class RegistersTest {
     @Test
     public void testRegister_SET_GET_ALL() {
         Registers registers = new Registers1();
-        for (int i = 0; i < 8; i++) {
-            registers.setRegister(i, new Word1(i * 100));
-        }
-
-        for (int i = 0; i < 8; i++) {
-            assertTrue(registers.getRegister(i).equals(new Word1(i * 100)));
-        }
+        registers.setRegister(0, new Word1(100));
+        registers.setRegister(1, new Word1(200));
+        registers.setRegister(2, new Word1(300));
+        registers.setRegister(3, new Word1(400));
+        registers.setRegister(4, new Word1(500));
+        registers.setRegister(5, new Word1(600));
+        registers.setRegister(6, new Word1(700));
+        registers.setRegister(7, new Word1(800));
+        
+        assertTrue(registers.getRegister(0).equals(new Word1(100)));
+        assertTrue(registers.getRegister(1).equals(new Word1(200)));
+        assertTrue(registers.getRegister(2).equals(new Word1(300)));
+        assertTrue(registers.getRegister(3).equals(new Word1(400)));
+        assertTrue(registers.getRegister(4).equals(new Word1(500)));
+        assertTrue(registers.getRegister(5).equals(new Word1(600)));
+        assertTrue(registers.getRegister(6).equals(new Word1(700)));
+        assertTrue(registers.getRegister(7).equals(new Word1(800)));
     }
 
     @Test
