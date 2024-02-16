53ef0cefb5d74b199b8017c13812f020c1d48511
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2024-02-16 17:08:25 -0500

    one more memory test

--- giles-nebulas-simulator/src/test/java/nebulas/simulator/MemoryTest.java
+++ giles-nebulas-simulator/src/test/java/nebulas/simulator/MemoryTest.java
@@ -1,6 +1,5 @@
 package nebulas.simulator;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,6 +9,15 @@ public class MemoryTest {
     @Test
     public void testMemory() {
         Word address = new Word1(0);
+        Memory memory = new Memory1();
+        memory.setWord(address, new Word1(10));
+
+        assertTrue(memory.getWord(address).equals(new Word1(10)));
+    }
+
+    @Test
+    public void testMemory_Addr_100() {
+        Word address = new Word1(100);
         Memory memory = new Memory1();
         memory.setWord(address, new Word1(10));
 
